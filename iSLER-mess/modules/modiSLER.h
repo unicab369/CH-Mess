2a3c097dfbc56719668b129484caf642f1b00d3b
uni369 <unicabpp@yahoo.com>
2025-08-24 15:07:00 -0400

    WORKING sender.

--- iSLER-mess/modules/modiSLER.h
+++ iSLER-mess/modules/modiSLER.h
@@ -72,57 +72,11 @@ void modiSLER_adv_data(MESS_DataFrame_t *dataFrame) {
 		.field_adv_flags = {0x02, 0x01, 0x06},
 		.name_len = 21,
 		.ad_type_local_name = 0x09,
-		.name = { 'b','e', 'e', '-', '6', '6', '6' },
+		.name = { 'b','e', 'e', '-', '2', '2', '2' },
 		.data_len = data_len+3,
 		.field_sev_data = {0xFF, 0xD7, 0x07},
 		.dataFrame = *dataFrame
 	};
-	
-
-	// uint8_t adv_data[MAX_PACKET_LEN];
-
-	// uint8_t dev_header[] = {
-    //     // MAC address
-    //     BLE_AD_MAC(0x112233445566),
-
-	// 	// ---- Advertising Flags Field ----
-    //     BLE_AD_FLAGS(0x06), // LE General Discoverable Mode, BR/EDR Not Supported
-		
-	// 	// ---- Complete Local Name Field  ----
-	// 	sizeof(dev_name) + 1, 		// Length: including the AD Type byte
-	// 	0x09, 						// AD Type: Complete Local Name
-	// };
-
-	// size_t adv_len = 0;
-
-	// //! copy from dev_header
-	// memcpy(&adv_data[adv_len], dev_header, sizeof(dev_header));
-	// adv_len += sizeof(dev_header);
-
-	// //! copy from dev_name
-	// memcpy(&adv_data[adv_len], dev_name, sizeof(dev_name));
-	// adv_len += sizeof(dev_name);
-
-	// //! copy from data_header
-	// uint8_t data_header[] = {
-	// 	// ---- Service Data Field ----
-	// 	data_len + 3, 				// Length: including the AD Type and Company ID bytes
-	// 	0xFF, 						// AD Type: Manufacturer Data
-	// 	0xD7, 0x07, 				// Company ID (WCH)
-	// };
-
-	// memcpy(&adv_data[adv_len], data_header, sizeof(data_header));
-	// adv_len += sizeof(data_header);
-
-	// //! copy from data
-	// memcpy(&adv_data[adv_len], data, data_len);
-	// adv_len += data_len;
-
-	// printf("adv len: %d\n", adv_len);
-
-	// for(int c = 0; c < sizeof(adv_channels); c++) {
-	// 	Frame_TX(adv_data, adv_len, adv_channels[c], PHY_MODE);
-	// }
 
 	PRINT_STRUCT_BYTES(&frame, "%02X");
 	printf("\n");
@@ -139,62 +93,30 @@ uint8_t modiSLER_print_rawData(uint8_t *data, size_t data_len) {
 uint8_t modiSLER_rx_handler() {
 	// The chip stores the incoming frame in LLE_BUF, defined in extralibs/iSLER.h
 	uint8_t *frame = (uint8_t*)LLE_BUF;
-	
-	uint8_t sender_mac[6];
-	for(int i= 0;i<6; i++) sender_mac[i] = frame[7-i];
+	iSLER_frame_t* rx_frame = (iSLER_frame_t*)(frame + 2);
 
-	uint8_t target_mac[] = { BLE_AD_MAC(0x112233445566) };
-
+	// uint8_t sender_mac[6];
+	// for(int i= 0;i<6; i++) sender_mac[i] = frame[7-i];
 	// printf("sender_mac\n");
 	// PRINT_ARRAY(sender_mac, "%02x");
+	// uint8_t target_mac[] = { BLE_AD_MAC(0x112233445566) };
 
-	if (memcmp(sender_mac, target_mac, 6) == 0) {
+	uint8_t target_mac[] = { 0x11, 0x22, 0x33, 0x44, 0x55, 0x66 };
+
+	if (memcmp(rx_frame->mac, target_mac, 6) == 0) {
 		// first 8 bytes contains: [RSSI x 1Byte] [len x 1Byte] [MAC x 6Bytes]
 		// The first two bytes of the frame are metadata with RSSI and length
 		PRINT_SEPARATOR();
 		printf("RSSI:%d len:%d MAC:", frame[0], frame[1]);
-		PRINT_ARRAY(sender_mac, "%02X");
-		printf("Raw Data: ");
-		PRINT_ARRAY_WITH_SIZE(frame, frame[1], "%02X");
+		PRINT_ARRAY(rx_frame->mac, "%02X");
+		// printf("Raw Data: ");
+		// PRINT_ARRAY_WITH_SIZE(frame, frame[1], "%02X");
 
-
-		iSLER_frame_t* rx_frame = (iSLER_frame_t*)LLE_BUF;
-
-		PRINT_STRUCT_BYTES(rx_frame, "%02X");
-
-		// uint8_t name_len_idx = 8+3;	// first 8 bytes + len(Advertising Flags Field)
-		// uint8_t name_len = frame[name_len_idx];
-		// printf("name idx: %u len: %u\n", name_len_idx, name_len);
-	
-		// uint8_t name_str_idx = name_len_idx + 2;	// AD Type: Local name 0x09
-		// printf("name str: ");
-		// for(int i= name_str_idx; i < name_str_idx + name_len - 1; i++) {
-		// 	printf("%c", frame[i]);
-		// }
-		// printf("\n");
-
-		// uint8_t tx_len_idx = name_str_idx + name_len - 1;
-		// uint8_t tx_len = frame[tx_len_idx];
-		// printf("\ntx idx: %u, len: %02X\n", tx_len_idx, tx_len);
-
-		// printf("tx data: ");
-		// // follow by AD Type and Company ID (3 bytes)
-		// for(int i= tx_len_idx + 4; i < tx_len_idx + 4 + tx_len; i++) {
-		// 	printf("%02X ", frame[i]);
-		// }
-		// printf("\n");
-
-		// printf("\nMESS_txFrame:\n");
-		// uint8_t frame_data[sizeof(MESS_DataFrame_t)] = {0};
-		// memcpy(frame_data, frame + tx_len_idx + 4, tx_len);
-
-		// // MESS_DataFrame_t receiv_frame;
-		// // memcpy(&receiv_frame, frame + tx_len_idx + 4, sizeof(MESS_DataFrame_t));
-		// // PRINT_STRUCT_BYTES(&receiv_frame, "%02X");
-
-		// MESS_DataFrame_t *rx = frame_data;
-		// PRINT_STRUCT_BYTES(rx, "%02X");
-
+		printf("preamble: %04X \n", rx_frame->dataFrame.preamble);
+		printf("controlbit: %04X \n", rx_frame->dataFrame.control_bits);
+		printf("msgCode: %04X \n", rx_frame->dataFrame.msgCode);
+		printf("groupId: %02X \n", rx_frame->dataFrame.group_id);
+		
 		return 1;
 	}
 
