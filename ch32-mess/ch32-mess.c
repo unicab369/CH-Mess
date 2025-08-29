4e487d5e163b4c6a75e84b3f660207f1c3b20ae1
uni369 <unicabpp@yahoo.com>
2025-08-29 00:24:59 -0400

    debugging ST7735.

--- ch32-mess/ch32-mess.c
+++ ch32-mess/ch32-mess.c
@@ -1,6 +1,7 @@
 #include "ch32fun.h"
 #include <stdio.h>
 
+#define BUTTON_PIN 		PD0
 
 #include "1_Foundation/fun_uart.h"
 #include "1_Foundation/modPWM.h"
@@ -57,43 +58,47 @@ int main()
 	funGpioInitAll();
 	Delay_Ms(100);
 
-	Button_t button_a = { PD0, BUTTON_IDLE, 0, 0, 0, 0, 0, 0 };
-	button_setup(&button_a);
+	Button_t button1 = { BUTTON_PIN, BUTTON_IDLE, 0, 0, 0, 0, 0, 0 };
+	button_setup(&button1);
+
+	// I2C1: uses PC1 & PC2
+	modI2C_setup();
+
+	uint8_t slave_mode = funDigitalRead(BUTTON_PIN);
+	if (slave_mode == 0) {
+		printf("I2C Slave mode\n");
+		SetupI2CSlave(0x77, i2c_registers, sizeof(i2c_registers), onI2C_SlaveWrite, onI2C_SlaveRead, false);
+	}
+	
+	// uses SCK-PC5, MOSI-PC6, RST-PD2, DC-PC4
+	SPI_init();
+	modST7735_setup(PC0, PC3);
 
 	// TIM2 Ch1, Ch2 : uses PD3, PD4.
 	// modEncoder_setup(&encoder_a);
 
-	// I2C1: uses PC1 & PC2
-	modI2C_setup();
-	SetupI2CSlave(0x77, i2c_registers, sizeof(i2c_registers), onI2C_SlaveWrite, onI2C_SlaveRead, false);
-
-	// uses SCK-PC5, MOSI-PC6, RST-PD2, CD-PC4
-	// SPI_init();
-	// modST7735_setup(PD2, PC4);
-
 	while(1) {
-		// uint8_t read = funDigitalRead(PD0);
-		// printf("reading: %d\n", read);
-
 		uint32_t now = millis();
 
-		button_run(&button_a, button_onChanged);
+		button_run(&button1, button_onChanged);
 		// modEncoder_task(now, &encoder_a, encoder_onChanged);
 
-		if (now - sec_time > 2000) {
+		if (now - sec_time > 1000) {
 			sec_time = now;
 
-			// modI2C_task(counter++);
-
+			if (slave_mode != 0) {
+				// modI2C_task(counter++);
+			}
+			
 			// // modJoystick_task();
 			// // dma_uart_tx(message, sizeof(message) - 1);
 
-			// uint32_t runtime_i2c = get_runTime(ssd1306_draw_test);
+			// uint32_t runtime_i2c = SysTick_getRunTime(ssd1306_draw_test);
 			// sprintf(str_output, "I2C runtime: %lu us", runtime_i2c);
 			// ssd1306_print_str_at(str_output, 0, 0);
 
-			// uint32_t runtime_tft = get_runTime(tft_test);
-			// printf("ST7735 runtime: %lu us\n", runtime_tft);
+			uint32_t runtime_tft = SysTick_getRunTime(tft_test);
+			printf("ST7735 runtime: %lu us\n", runtime_tft);
 
 			// storage_test();
 		}
